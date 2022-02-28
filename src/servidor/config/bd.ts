885566c0d3327b9f0a1321b1c152122a63760784
saulrodriguezcceo <saulrodriguez221299@gmail.com>
2022-02-28 07:42:57 -0600

    Base de datos local

--- src/servidor/config/bd.ts
+++ src/servidor/config/bd.ts
@@ -1,4 +1,4 @@
 // const user = 'admin';
 // const password = 'JN2TXi4r4ee09fHp';
 // const database = "checador-uaa"
-export const BD_URL = `mongodb+srv://saul:Saul1234@cluster0.etcaq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+export const BD_URL = `mongodb://localhost:27017/escuelas`;
